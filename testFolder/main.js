9e88bce802c9fa57b07e74050a6db03d36ed15ac
xqwtsz <xqwtsz@gmail.com>
2018-12-04 16:15:03 +0100

    sidebar tests

--- testFolder/main.js
+++ testFolder/main.js
@@ -1,46 +1,39 @@
-let key = "c90d69bab6e84e39ac36904e19c7fbdd";
-let url =
-  "https://newsapi.org/v2/top-headlines?sources=die-zeit&apiKey=c90d69bab6e84e39ac36904e19c7fbdd";
-// https://newsapi.org/v2/top-headlines?sources=bbc-news&apiKey=c90d69bab6e84e39ac36904e19c7fbdd
-const getNews = async function(url) {
-  let raw = await fetch(url);
-  let data = await raw.json();
-  let mainRow = document.getElementById("mainRow");
-  let miniRow = document.getElementById("miniRow");
+$(document).ready(function() {
+  // Handler for .ready() called.
 
-  if (data.status === "ok") {
-    console.log(data.articles);
+  let key = "c90d69bab6e84e39ac36904e19c7fbdd";
+  let url =
+    "https://newsapi.org/v2/top-headlines?sources=bbc-news&apiKey=c90d69bab6e84e39ac36904e19c7fbdd";
+  // https://newsapi.org/v2/top-headlines?sources=bbc-news&apiKey=c90d69bab6e84e39ac36904e19c7fbdd
+  const getNews = async function(url) {
+    let raw = await fetch(url);
+    let data = await raw.json();
+    let mainRow = document.getElementById("cards");
+    let miniRow = document.getElementById("headlines");
 
-    let summaries = data.articles.map(function(item) {
-      return (item = `
+    if (data.status === "ok") {
+      //console.log(data.articles);
+
+      let summaries = data.articles.map(function(item) {
+        return (item = `
       <a href="${
         item.url
       }" target="_blank"  class="list-group-item list-group-item-action flex-column align-items-start">
       <div class="d-flex w-100 justify-content-between">
-        <strong><h5 class="mb-1">${item.title}</h5></strong>
+        <strong><h3 class="mb-1">${item.title}</h3></strong>
         <small>${item.publishedAt.slice(0, 10)}</small>
       </div>
       <p class="mb-1">${item.description}</p>
-      <small>${item.author}</small>
+      <!--<small>${item.author}</small>-->
     </a>`);
-    });
+      });
 
-    /*
-    <a href="#" class="list-group-item list-group-item-action flex-column align-items-start active">
-    <div class="d-flex w-100 justify-content-between">
-      <h5 class="mb-1">List group item heading</h5>
-      <small>3 days ago</small>
-    </div>
-    <p class="mb-1">Donec id elit non mi porta gravida at eget metus. Maecenas sed diam eget risus varius blandit.</p>
-    <small>Donec id elit non mi porta.</small>
-  </a>*/
-
-    let headlines = data.articles.map(function(item) {
-      if (!item.urlToImage) {
-        item.urlToImage =
-          "https://dummyimage.com/300x200/000/010105&text=No+image+provided.";
-      }
-      return (item = `<div class="col-md-4">
+      let headlines = data.articles.map(function(item) {
+        if (!item.urlToImage) {
+          item.urlToImage =
+            "https://dummyimage.com/300x200/000/010105&text=No+image+provided.";
+        }
+        return (item = `<div class="col-4">
                         <div class="card mb-4 shadow-sm">
                           <img
                             class="card-img-top"
@@ -71,25 +64,25 @@ const getNews = async function(url) {
                         </div>
                       </div>
                     </div>`);
-    });
-    console.log(headlines);
+      });
+      //console.log(headlines);
 
-    headlines.forEach(element => {
-      mainRow.innerHTML += element;
-    });
+      headlines.forEach(element => {
+        mainRow.innerHTML += element;
+      });
 
-    summaries.forEach(element => {
-      miniRow.innerHTML += element;
-    });
-  } else {
-    console.log("There was an error.");
-  }
-  //console.log(data);
-};
+      summaries.forEach(element => {
+        miniRow.innerHTML += element;
+      });
+    } else {
+      console.log("There was an error.");
+    }
+    //console.log(data);
+  };
 
-getNews(url);
+  getNews(url);
 
-/*
+  /*
 0:
 author: "BBC News"
 content: "Media caption Footage of the collision was posted by the Ukrainian interior minister Russian President Vladimir Putin has accused Ukraine's leader, Petro Poroshenko, of trying to boost his ratings ahead of 2019 elections with a naval confrontation off Crimea.… [+298 chars]"
@@ -100,3 +93,36 @@ title: "Sea clash staged by Ukraine, says Putin"
 url: "http://www.bbc.co.uk/news/world-europe-46370619"
 urlToImage: "https://ichef.bbci.co.uk/images/ic/1024x576/p06swhnz.jpg"
 */
+
+  //testing the hamburger menu button, very broken atm
+  /*
+function openSideBar() {
+  let sideBar = document.getElementById("mySideBar");
+  console.log("button clicked");
+  if (sideBar.style.display === "none") {
+    console.log("if runs");
+    sideBar.style.display = "block";
+  } else {
+    console.log("else runs");
+    sideBar.style.display = "none";
+  }
+}*/
+  $("#menuButton").click(function() {
+    console.log("button clicked");
+    $("#mySideBar").toggle();
+  });
+
+  $(document).click(function(e) {
+    let sideMenuButton = $("#hamburgerMenu");
+    console.log(e.target.id);
+    let sideBar = $("#mySideBar");
+    console.log(sideBar[0].id);
+    let sideBarID = sideBar[0].id;
+    let sideMenuButtonID = sideMenuButton[0].id;
+
+    if (sideBarID !== e.target.id && sideMenuButtonID !== e.target.id) {
+      console.log("if ran");
+      sideBar.hide();
+    }
+  });
+});

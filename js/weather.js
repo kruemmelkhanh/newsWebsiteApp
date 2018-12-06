f10deaa98f3a944c7572c738759e887a48f53248
Stefan Tolksdorf <stefan.tolksdorf@gmail.com>
2018-12-06 15:56:17 +0100

    simple 5 day forecast added

--- js/weather.js
+++ js/weather.js
@@ -5,6 +5,11 @@ var temperature = document.getElementById("temperature");
 var icon = document.getElementById("weatherIcon");
 var todayMax = document.getElementById("todayMax");
 var todayMin = document.getElementById("todayMin");
+var forecast1 = document.getElementById("forecast1");
+var forecast2 = document.getElementById("forecast2");
+var forecast3 = document.getElementById("forecast3");
+var forecast4 = document.getElementById("forecast4");
+var forecast5 = document.getElementById("forecast5");
 
 function getLocation() {
   if (navigator.geolocation) {
@@ -39,10 +44,15 @@ async function getWeather(userLat, userLong) {
 }
 
 async function getForecast(userLat, userLong) {
-  var myURL = `https://api.openweathermap.org/data/2.5/forecast/daily?lat=${userLat}&lon=${userLong}&cnt=3&APPID=${key}&units=metric`;
+  var myURL = `https://api.openweathermap.org/data/2.5/forecast/daily?lat=${userLat}&lon=${userLong}&cnt=6&APPID=${key}&units=metric`;
   let raw = await fetch(myURL);
   let data = await raw.json();
-  console.log(data);
+  // console.log(data);
   todayMax.innerHTML = "↑ " + data.list[0].temp.max.toFixed(1);
   todayMin.innerHTML = "↓ " + data.list[0].temp.min.toFixed(1);
+  forecast1.innerHTML = data.list[1].temp.max.toFixed(1);
+  forecast2.innerHTML = data.list[2].temp.max.toFixed(1);
+  forecast3.innerHTML = data.list[3].temp.max.toFixed(1);
+  forecast4.innerHTML = data.list[4].temp.max.toFixed(1);
+  forecast5.innerHTML = data.list[5].temp.max.toFixed(1);
 }
